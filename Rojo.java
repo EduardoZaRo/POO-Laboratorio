e80a660e461f025475fe0fae22d44087ac860c11
EduardoZaRo <71353005+EduardoZaRo@users.noreply.github.com>
2021-05-18 07:51:42 -0700

    Delete Rojo.java

--- Rojo.java
+++
@@ -1,6 +0,0 @@
-public class Rojo extends Pajaro{
-	String nombre = "Rojo", poder = "Cabezaso", tamano = "Chico", color = "Rojo";
-	public static void ataque(){
-		System.out.println("Rojo a dado un cabezaso!");
-	}
-}
