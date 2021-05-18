a1f3270f3d8d9d2c8d7d921e84cd6522544bc096
EduardoZaRo <71353005+EduardoZaRo@users.noreply.github.com>
2021-05-18 07:51:27 -0700

    Delete BombBird.java

--- BombBird.java
+++
@@ -1,7 +0,0 @@
-public class BombBird extends Pajaro{
-	String nombre = "BombBird", poder = "Explotar", tamano = "Grande", color = "Negro";
-	public static void ataque(){
-		System.out.println("BombBird a impactado y explotado!");
-	}
-
-}
