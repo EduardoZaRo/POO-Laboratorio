b7579584b869ee5645e4f6501ae28949acbab12a
EduardoZaRo <71353005+EduardoZaRo@users.noreply.github.com>
2021-05-18 07:51:21 -0700

    Delete Amarillo.java

--- Amarillo.java
+++
@@ -1,7 +0,0 @@
-public class Amarillo extends Pajaro{
-	String nombre = "Amarillo", poder = "Veloz", tamano = "Chico", color = "Amarillo";
-	public static void ataque(){
-		System.out.println("Amarillo acelero y estampo en la estructura!");
-	}
-
-}
